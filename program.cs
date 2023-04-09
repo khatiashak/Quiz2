2b560c3ca5432dcb43077c24b6ba1d4068772366
khatiashak <118073598+khatiashak@users.noreply.github.com>
2023-04-09 18:33:44 +0400

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,49 @@
+//First exercise
+
+Console.WriteLine("enter the size of an array, preferably less than 10 so that it could be tested easily: ");
+int size = int.Parse(Console.ReadLine());
+
+int[] array = new int[size];
+
+Console.WriteLine("Now write the elements of your array: ");
+
+for (int i = 0; i < size; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+
+int sumOddNumb = 0;
+int prodEvenNumb = 1;
+foreach (int element in array)
+{
+    if (element % 2 == 0)
+    {
+        sumOddNumb += element;
+    }
+    else
+    {
+        prodEvenNumb *= element;
+    }
+}
+
+Dictionary<int, int> counts = new Dictionary<int, int>();
+
+foreach (int element in array)
+{
+    if (counts.ContainsKey(element))
+    {
+        counts[element]++;
+    }
+    else
+    {
+        counts[element] = 1;
+    }
+}
+
+Console.WriteLine($"There are {sumOddNumb} odd and {prodEvenNumb} even numbers");
+
+foreach (KeyValuePair<int, int> pair in counts)
+{
+    Console.WriteLine($"Element {pair.Key} occurs {pair.Value} times.");
+}
